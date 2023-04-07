ad46269fa5743c11f506e5bc73527a418966eb1e
Sam <samnewell91@gmail.com>
2023-04-07 16:08:49 +0100

    Reset button & animation
    Added a ball shake animation and a reset button with the intent on making the button and input inactive after a question has been asked. Reset button will empty the input box.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const shakeButton = document.getElementById('shakeBall');
 const result = document.getElementById('result');
+const outerBall = document.querySelector('.outerBall');
+const innerBall = document.querySelector('.innerBall');
+let ballAnswer = ""
 const choices = ["It is certain",
     "Probably<br>not",
     "Without<br>a<br>doubt",
@@ -11,13 +14,26 @@ const choices = ["It is certain",
     "Ask<br>again<br>later",
     "Don't count<br>on it",
     "For<br>certain",
+    "Never",
+    "Maybe",
+    "Not on this<br>planet",
 ]
-let ballAnswer = ""
+
+
+// Generate a random answer from the list of choices
 
 function shakeAnswer() {
     ballAnswer = choices[Math.floor(Math.random()*choices.length)];
     result.innerHTML = ballAnswer;
 }
 
-shakeButton.addEventListener('click', shakeAnswer);
+// Function for Shaking the ball
 
+function ballShake() {
+    outerBall.classList.add('shake');
+    innerBall.classList.add('shake');
+    
+}
+
+shakeButton.addEventListener('click', shakeAnswer,);
+shakeButton.addEventListener('click', ballShake,);
